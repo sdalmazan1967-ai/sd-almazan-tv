4ad3b589ec5817772bb02738f76fc6820895038a
sdalmazan1967-ai <sdalmazan1967@gmail.com>
2026-06-04 14:06:10 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,4 @@ const SUPABASE_URL = 'https://sdscrqjyzvavreschiba.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_QvazS7O_bpqhu8kEHYYYRw_vE_SG628';
 const ADMIN_EMAIL = 'sdalmazan1967@gmail.com';
 
-if (!window._supabaseClient) {
-  window._supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-}
-const supabase = window._supabaseClient;
+const sbCliente = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
